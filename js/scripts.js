89f750c2f3d79af28110505e557c94c65ba19d95
Brett New and Charles Ewel <charles@>
2016-06-08 15:55:14 -0700

    It will have functionality to convert binary, trinary, or hexadecimal numbers to decimal numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
-  $("form#blank").submit(function(event) {
+  $("#submit-binary").click(function(event) {
     event.preventDefault();
-    var binarynumber = $("input#number").val();
+    var binarynumber = $("input#number-binary").val();
     var decimaloutput = binarytodecimal(binarynumber);
     $("#result").text(decimaloutput);
+
+  });
+  $("#submit-trinary").click(function(event){
+    event.preventDefault();
+    var binarynumber = $("input#number-binary").val();
+    var decimaloutput2 = trinarytodecimal(binarynumber);
+    $("#result").text(decimaloutput2);
+  });
+  $("#submit-hexadecimal").click(function(event){
+    event.preventDefault();
+    var binarynumber = $("input#number-binary").val();
+    var decimaloutput2 = hextodecimal(binarynumber);
+    $("#result").text(decimaloutput2);
   });
 });
 
@@ -13,6 +26,45 @@ var binarytodecimal = function(binarynumber) {
   console.log(binaryarray)
   for(var index=binaryarray.length-1; index>=0; index--){
     decimalnumber+=binaryarray[index]*Math.pow(2,binaryarray.length-1-index);
+    console.log(decimalnumber)
+  }
+  return decimalnumber
+}
+
+var trinarytodecimal = function(binarynumber) {
+  var decimalnumber=0;
+  var binaryarray=binarynumber.split("");
+  console.log(binaryarray)
+  for(var index=binaryarray.length-1; index>=0; index--){
+    decimalnumber+=binaryarray[index]*Math.pow(3,binaryarray.length-1-index);
+    console.log(decimalnumber)
+  }
+  return decimalnumber
+}
+
+var hextodecimal = function(number) {
+  var decimalnumber=0;
+  var array=number.split("");
+  var newarray=array.map(function(value){
+    if(value==="a"){
+      return 10;
+    } else if(value==="b"){
+      return 11;
+    } else if(value==="c"){
+      return 12;
+    } else if(value==="d"){
+      return 13;
+    } else if(value==="e"){
+      return 14;
+    } else if(value==="f"){
+      return 15;
+    } else {
+      return value;
+    }
+  });
+  console.log(array)
+  for(var index=newarray.length-1; index>=0; index--){
+    decimalnumber+=newarray[index]*Math.pow(16,newarray.length-1-index);
     console.log(decimalnumber)
   }
   return decimalnumber
